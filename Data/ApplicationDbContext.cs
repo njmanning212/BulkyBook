4f6449c41336f1c36f5cee0fc3fda4440a205db7
Nicholas Manning <njmanning212@gmail.com>
2023-10-17 13:12:30 -0500

    create category table migrations, allow use of env file for database connection string"

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookVSC;
+
+public class ApplicationDbContext : DbContext
+{
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+  {
+  }
+
+  public DbSet<Category> Categories { get; set; }
+  
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using BulkyBookVSC;
+using dotenv.net;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotEnv.Load();
+var server = Environment.GetEnvironmentVariable("DB_SERVER");
+var database = Environment.GetEnvironmentVariable("DB_DATABASE");
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var trustServerCert = Environment.GetEnvironmentVariable("DB_TRUST_SERVER_CERT");
+
+var connectionString = $"Server={server};Database={database};User Id={user};Password={password};TrustServerCertificate={trustServerCert};";
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
