6c1fb7c934fe8f8c2162fc077c7263c1bc119908
Nicholas Manning <njmanning212@gmail.com>
2023-10-17 14:20:23 -0500

    create category controller, view folder, and index.cshtml

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BulkyBookVSC.Models;
+
+namespace BulkyBookVSC;
+
+public class CategoryController: Controller
+{
+  public IActionResult Index ()
+  {
+    return View();
+  }
+}
