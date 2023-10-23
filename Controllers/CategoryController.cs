e139ae064b0a9466b747157a9db1f17df4ae2597
Nicholas Manning <njmanning212@gmail.com>
2023-10-23 14:40:47 -0500

    add post request controller to allow user to add a category and display order

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -4,23 +4,32 @@ using BulkyBookVSC.Models;
 
 namespace BulkyBookVSC;
 
-public class CategoryController: Controller
+public class CategoryController : Controller
 {
   private readonly ApplicationDbContext _db;
 
   public CategoryController(ApplicationDbContext db)
   {
     _db = db;
-    
+
   }
-  public IActionResult Index ()
+  public IActionResult Index()
   {
     IEnumerable<Category> objCategoryList = _db.Categories;
     return View(objCategoryList);
   }
 
-  public IActionResult Create ()
+  public IActionResult Create()
   {
     return View();
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public IActionResult Create(Category obj)
+  {
+    _db.Categories.Add(obj);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
   }
 }
