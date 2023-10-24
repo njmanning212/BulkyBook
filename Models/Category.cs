6cfa0958095cda402cd84347c581e1eab0206918
Nicholas Manning <njmanning212@gmail.com>
2023-10-24 11:53:51 -0500

    set display name of display order, add range for display order

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookVSC;
 
@@ -8,6 +9,8 @@ public class Category
   public int Id { get; set; }
   [Required]
   public string Name { get; set; }
+  [DisplayName("Display Order")]
+  [Range(1, 100, ErrorMessage = "Display Order for category must be from 1 to 100.")]
   public int DisplayOrder { get; set; }
   public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 }
