99e9691afbf64471c5d8193c61f5c6239d54d85e
Nicholas Manning <njmanning212@gmail.com>
2023-10-24 12:14:36 -0500

    add edit show page UI on index page and controller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -40,4 +40,38 @@ public class CategoryController : Controller
     }
     return View(obj);
   }
+
+  public IActionResult Edit(int? id)
+  {
+    if (id == null || id == 0)
+    {
+      return NotFound();
+    }
+
+    var categoryFromDb = _db.Categories.Find(id);
+
+    if (categoryFromDb == null)
+    {
+      return NotFound();
+    }
+
+    return View(categoryFromDb);
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public IActionResult Edit(Category obj)
+  {
+    if(obj.Name == obj.DisplayOrder.ToString())
+    {
+      ModelState.AddModelError("name", "The Display Order cannot be the same as the Name.");
+    }
+    if (ModelState.IsValid)
+    {
+      _db.Categories.Add(obj);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    return View(obj);
+  }
 }
