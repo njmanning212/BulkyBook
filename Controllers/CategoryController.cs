af2723cf8b3bbbf7198eb39e4fbd665d740622d4
Nicholas Manning <njmanning212@gmail.com>
2023-10-24 12:57:09 -0500

    utilize tempData in order to communicate to user successful creation, updating, or deleting of category on index page

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -36,6 +36,7 @@ public class CategoryController : Controller
     {
       _db.Categories.Add(obj);
       _db.SaveChanges();
+      TempData["success"] = "Category has been successfully created.";
       return RedirectToAction("Index");
     }
     return View(obj);
@@ -70,6 +71,8 @@ public class CategoryController : Controller
     {
       _db.Categories.Update(obj);
       _db.SaveChanges();
+      TempData["success"] = "Category has been successfully updated.";
+
       return RedirectToAction("Index");
     }
     return View(obj);
@@ -105,6 +108,8 @@ public class CategoryController : Controller
 
     _db.Categories.Remove(obj);
     _db.SaveChanges();
+    TempData["success"] = "Category has been successfully Deleted.";
+
     return RedirectToAction("Index");
 
   }
